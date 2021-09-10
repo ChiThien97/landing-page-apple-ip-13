a4fb9fac4f83a395a5d2b9326cb553a2abd58c7b
Chí Thiện <chithien300313@gmail.com>
2021-09-10 11:32:56 +0700

    Third commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
 function switchTabProducts(id) {
-    $('.tab-'+id).addClass('active');
-    $('.tab-'+id).siblings().removeClass('active');
+    $('.tab-' + id).addClass('active');
+    $('.tab-' + id).siblings().removeClass('active');
     $('.content').addClass('d-none')
-    $('#'+id).removeClass('d-none');
-
+    $('#' + id).removeClass('d-none');
 }
+
+var player = videojs('my_video_1');
+player.play();
+
+$(document).ready(function ($) {
+    $('#my-form').submit(function (e) {
+        e.preventDefault();
+        var name = $('#name').val().trim();
+        var phone = $('#phone').val().trim();
+        var email = $('#email').val().trim();
+        var regex = /((09|03|07|08|05)+([0-9]{8})\b)/i;
+        if (phone.length > 10 || phone.length == 0) {
+            alert("Quý khách vui lòng kiểm tra lại số điện thoại!");
+        } else if (regex.test(phone) == false) {
+            alert("Quý khách vui lòng kiểm tra lại số điện thoại!");
+        } else {
+            $.ajax({
+                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeRkAVK4nx5I01ALRbFpyg2ymL7tI2hwOpOYkaPrWAN3CWPcg/formResponse",
+                data: $('#my-form').serialize(),
+                type: "POST",
+                dataType: "json"
+            });
+            $('#name').addClass('d-none');
+            $('#phone').addClass('d-none');
+            $('#email').addClass('d-none');
+            $('#my-form>button').addClass('d-none');
+            $('#submit-success').removeClass('d-none');
+        }
+        return false;
+    });
+})
