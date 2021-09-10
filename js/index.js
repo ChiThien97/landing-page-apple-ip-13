9b8bc88c59bc3e029a8a79d6f024c1f3382eb7cb
Chí Thiện <chithien300313@gmail.com>
2021-09-10 19:18:39 +0700

    22 commit

--- js/index.js
+++ js/index.js
@@ -20,6 +20,9 @@ var player = videojs('my_video_1');
 player.play();
 
 $(document).ready(function ($) {
+    setTimeout(function(){
+        $('#zalo').hide()
+        }, 3000);
     $('#my-form').submit(function (e) {
         e.preventDefault();
         var name = $('#name').val().trim();
